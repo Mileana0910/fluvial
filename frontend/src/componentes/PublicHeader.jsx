95bf418f8a7a539bbbba6dc64f2f48a90ec31761
Ana Milena Vasquez Tovar <mileana0910@gmail.com>
2025-11-10 15:43:07 -0500

    conexión con el backend
    módulo privado

--- frontend/src/componentes/PublicHeader.jsx
+++ frontend/src/componentes/PublicHeader.jsx
@@ -31,11 +31,8 @@ export default function PublicHeader() {
         <Link to="/historia" className="hover:underline">Historia</Link>
         <Link to="/contacto" className="hover:underline">Contacto</Link>
       </nav>
-       {/* Ruta Gabriel - URL externa */}
       <a
-        href="https://catamaranbackend.onrender.com/login.html" 
-        target="_blank"
-        rel="noopener noreferrer"
+        href="/login"
         className="border border-blue-500 text-blue-500 px-4 py-2 rounded-full font-semibold hover:bg-blue-50 transition hidden md:block"
       >
         Iniciar Sesión
@@ -55,9 +52,7 @@ export default function PublicHeader() {
           <Link to="/historia" className="hover:underline" onClick={() => setIsOpen(false)}>Historia</Link>
           <Link to="/contacto" className="hover:underline" onClick={() => setIsOpen(false)}>Contacto</Link>
           <a
-            href="https://catamaranbackend.onrender.com/login.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/login"
             className="border border-blue-500 text-blue-500 px-4 py-2 rounded-full font-semibold hover:bg-blue-50 transition"
           >
             Iniciar Sesión
